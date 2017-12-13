d66c30ddb0fb9d9fd8b04c3222b7100c94136ea6
Tauqeer1 <tauqeer.shakir@yahoo.com>
2017-12-13 13:39:33 +0500

    task5 solved

--- task5.js
+++ task5.js
@@ -1,12 +1,25 @@
-let makeConsecutive2 = (array) => {
-    let count = 0;
-    let sortedArray = array.sort((a,b) => { return a - b; });
-    console.log('sortedArray', sortedArray);
-    let min = sortedArray[0];
-    let max = sortedArray[array.length-1];
-    console.log('min',min);
-    console.log('max', max);
-    count = max - min + 1 - sortedArray.length;
+/* 
+    Copied solution
+function makeArrayConsecutive2(statues) {
+    var count = 0;
+    var sortedArray = statues.sort((a, b) => a - b);
+
+    return (sortedArray[sortedArray.length - 1] - sortedArray[0] + 1) - sortedArray.length;
+}
+makeArrayConsecutive2([6, 2, 3, 8])
+ */
+
+// Own solution
+let makeConsecutive2 = (statues) => {
+    let sortedStatues = statues.sort((a, b) => a - b);
+    let count = 0, j = 0;
+    for (let i = sortedStatues[0]; i <= sortedStatues[sortedStatues.length - 1]; i++) {
+        if (sortedStatues[j] === undefined) {
+            count++;
+        }
+        j++;
+    }
     return count;
 };
-console.log(makeConsecutive2([4,2,7,18]));
+console.log(makeConsecutive2([4, 2, 7, 18]));
+
