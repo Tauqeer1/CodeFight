ab1a63c3f72b9a2eed5275cc4ea045a574eb23d2
Tauqeer1 <tauqeer.shakir@yahoo.com>
2017-12-13 16:30:32 +0500

    task6 completed

--- task6.js
+++ task6.js
@@ -1,33 +1,17 @@
 /* 
-
-Given a sequence of integers as an array, determine whether it is possible to 
-obtain a strictly increasing sequence by removing no more than one element 
-from the array.
-
-Example
-For sequence = [1, 3, 2, 1], the output should be
-almostIncreasingSequence(sequence) = false;
-There is no one element in this array that can be removed in 
-order to get a strictly increasing sequence.
-For sequence = [1, 3, 2], the output should be
-almostIncreasingSequence(sequence) = true.
-
-You can remove 3 from the array to get the strictly increasing sequence [1, 2]. 
-Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
-
-Input/Output
-
-[time limit] 4000ms (js)
-
-[input] array.integer sequence
-
-Guaranteed constraints:
-2 ≤ sequence.length ≤ 105,
--105 ≤ sequence[i] ≤ 105.
-
-[output] boolean
-
-Return true if it is possible to remove one element from the array in order to get 
-a strictly increasing sequence, otherwise return false.
-
+Using the JavaScript language, have the 
+function TimeConvert(num) take the num parameter being passed and return the number
+ of hours and minutes the parameter converts to (ie. if num = 63 then the output should 
+be 1:3). Separate the number of hours and minutes with a colon.  
 */
+
+const timeConvert = (num) => {
+    let hour = (num / 60).toString().split(".")[0];
+    let min = ((num % 60).toString().length === 1) ? ('0' + (num % 60).toString()) : (num % 60).toString();
+    let time = hour + ':' + min;
+    return time;
+}
+
+console.log(timeConvert(60));
+
+// 3 * 60 = 180 + 20
