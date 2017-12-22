c266cabdd0e017a1a9ded1201391039c42d5aab3
Tauqeer1 <tauqeer.shakir@yahoo.com>
2017-12-22 18:41:57 +0500

    working on task12

--- task12.js
+++ task12.js
@@ -22,10 +22,47 @@ The sorted list.
  */
 
 /* My Solution */
-// Passed custom test, but failed to pass hidden test
+// Passed sample test, but failed to pass hidden test
 let sortByZ = (g, z) => {
     z -= 1;
     return g.sort((a, b) => a.charCodeAt(z) - b.charCodeAt(z));
 };
-console.log(sortByZ(g, 3));
+// console.log(sortByZ(["Cow", "Chicken", "Deer", "Rabbit"], 3));
 
+/* Other's solutions */
+i = 9
+SortByZ = (g, z) => {
+    g.map(w => {
+        console.log('w', w);
+        console.log('Buffer(w)', Buffer(w));
+        console.log('wz', Buffer(w)[z - 1]);
+        console.log('wz1', [Buffer(w)[z - 1] & 95, ++i, w]);
+        [Buffer(w)[z - 1]]
+    })
+    return g.map(w => [Buffer(w)[z - 1] & 95, ++i, w]).sort().map(a => a[2])
+}
+
+// console.log(SortByZ(["Cow", "Chicken", "Deer", "Rabbit"], 3));
+
+let str = 'Cow';
+let z = 3;
+// console.log('Buffer(str)', Buffer('abc'));
+// console.log('z', z);
+// console.log('Buffer(str)[z]', Buffer('abc')[0]);
+let newBuffer = Buffer(str);
+// console.log('newBuffer', newBuffer);
+// console.log('newBuffer', typeof newBuffer);
+
+let str1 = 'abcdefghijklmnopqrstuvwxyz';
+let str2 = 'def';
+
+// console.log(Buffer('b')[0] - 97);
+// console.log(Buffer(str1)[3] % 32);
+str1.split('').map(ch => Buffer(ch)[0] - 96);
+// console.log(str1.map(ch => Buffer(ch)[0] - 97));
+
+let st = 'Abc';
+let st1 = 'Abc';
+let name1 = "Bob", name2 = "Dan";
+console.log('Buffer(st)', eval(Buffer(name1.toLowerCase()).join('+')));
+console.log('Buffer(st)', eval(Buffer(name2.toLowerCase()).join('+')));
